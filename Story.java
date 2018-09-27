05f1f7da19798ec62b7bce98e31edf0381b11102
lkgross <profgross@gmail.com>
2018-09-27 08:46:06 -0400

    Updating Story.java

--- Story.java
+++ Story.java
@@ -1,51 +1,60 @@
-
+package com.company;
 
 import java.util.Scanner;
+
+/*
+ * Story.java
+ *
+ * A class for further practice with user input
+ *
+ * Starter code by Laura K. Gross, COMP 152, Bridgewater State University
+ *
+ * Completed by: [student name], [student email]
+ *         date: [date of completion]
+ *
+ */
 
 public class Story {
     public static void main(String[] args) {
 
         String name, city, college, profession, animal, pet;
+        // Use integer type if you want to manipulate age as a number.
         int age;
 
-        //Scanner object to read user input
+        // Create Scanner object to read user input.
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Provide a first name: ");
+        System.out.println("Provide a first name: ");
         name = keyboard.nextLine();
 
-        System.out.print("Provide an age: ");
+        System.out.println("Provide an age: ");
         age = keyboard.nextInt();
 
-        // To remove the newline remaining
+        // Consume the newline character.
         keyboard.nextLine();
 
-        System.out.print("Provide the name of a city: ");
+        System.out.println("Provide the name of a city: ");
         city = keyboard.nextLine();
 
-        System.out.print("Provide the name of a college: ");
+        System.out.println("Provide the name of a college: ");
         college = keyboard.nextLine();
 
-        System.out.print("Provide the name of a profession: ");
+        System.out.println("Provide the name of a profession: ");
         profession = keyboard.nextLine();
 
-        System.out.print("State a type of animal: ");
+        System.out.println("State a type of animal: ");
         animal = keyboard.nextLine();
 
-        System.out.print("State a pet's name: ");
+        System.out.println("State a pet's name: ");
         pet = keyboard.nextLine();
 
-        System.out.println();
+        System.out.println(); // Leave a blank line.
 
-        System.out.print("There once was a person named " + name + " who lived in " + city + ".");
-        System.out.println();
-        System.out.print("At the age of " + age + ", " + name + " went to college at " + college
+        System.out.println("There once was a person named " + name + " who lived in " + city + ".");
+        System.out.println("At the age of " + age + ", " + name + " went to college at " + college
                 + ".");
-        System.out.println();
-        System.out.print(name + " graduated and went to work as a " + profession + ".");
-        System.out.println();
-        System.out.print("Then, " + name + " adopted a " + animal + " named " + pet + ".");
-        System.out.println();
+        System.out.println(name + " graduated and went to work as a " + profession + ".");
+        System.out.println("Then, " + name + " adopted a " + animal + " named " + pet + ".");
         System.out.print("They lived happily ever after!");
 
     }

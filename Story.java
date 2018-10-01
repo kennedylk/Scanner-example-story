8081a40b5ce13229d2cff6f655edb0c0aa435791
lkgross <profgross@gmail.com>
2018-09-30 23:21:12 -0400

    Check for appropriate article "a" or "an" in a static method.

--- Story.java
+++ Story.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /*
  * Story.java
  *
- * A class for further practice with user input
+ * A class for further practice with user input and static methods
  *
  * Starter code by Laura K. Gross, COMP 152, Bridgewater State University
  *
@@ -15,6 +15,18 @@ import java.util.Scanner;
  */
 
 public class Story {
+
+    public static String article(String word) {
+        char firstChar = word.charAt(0);
+        if (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o'
+                || firstChar == 'u' || firstChar == 'A' || firstChar == 'E' || firstChar == 'I' || firstChar == 'O'
+                || firstChar == 'U') {
+            return "an";
+        } else {
+            return "a";
+        }
+    }
+
     public static void main(String[] args) {
 
         String name, city, college, profession, animal, pet;
@@ -26,11 +38,14 @@ public class Story {
 
         System.out.println("Provide a first name: ");
         name = keyboard.nextLine();
+        // Note that nextLine reads the whole line the user enters, including the newline character.
 
         System.out.println("Provide an age: ");
         age = keyboard.nextInt();
+        // Note that nextInt reads only the next integer.  It does not read the newline character.
+        // The newline character from when the user hit enter is waiting to be read.
 
-        // Consume the newline character.
+        // Consume the newline character explicitly.
         keyboard.nextLine();
 
         System.out.println("Provide the name of a city: ");
@@ -53,10 +68,11 @@ public class Story {
         System.out.println("There once was a person named " + name + " who lived in " + city + ".");
         System.out.println("At the age of " + age + ", " + name + " went to college at " + college
                 + ".");
-        System.out.println(name + " graduated and went to work as a " + profession + ".");
-        System.out.println("Then, " + name + " adopted a " + animal + " named " + pet + ".");
+        System.out.println(name + " graduated and went to work as " + article(profession) + " " +
+                profession + ".");
+        System.out.println("Then, " + name + " adopted " + article(animal) + " " + animal
+                + " named " + pet + ".");
         System.out.print("They lived happily ever after!");
 
     }
 }
-
